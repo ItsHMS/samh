df1583f8349227793526c1299829bb6b590d90b1
iamwaleedmirza <mirzawaleedbaig778@gmail.com>
2024-09-29 23:46:19 +0500

    refactor(i18n): improved direction handling for RTL/LTR support

--- app.vue
+++ app.vue
@@ -3,10 +3,10 @@
 <script setup>
 const { t, locale } = useI18n();
 console.log("Current locale:", locale.value);
-
+const isRTL = computed(() => locale.value === "ar");
 useHead(() => ({
   htmlAttrs: {
-    dir: "rtl",
+    dir: isRTL.value ? "rtl" : "ltr",
   },
   title: locale.value === "ar" ? "مرحبًا" : "Welcome",
 }));
