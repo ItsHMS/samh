c8740cacc69f7fcdf4871fbbe30304996401d8b9
iamwaleedmirza <mirzawaleedbaig778@gmail.com>
2024-09-29 23:39:52 +0500

    fix(i18n): ensured consistent RTL direction on initial load

--- app.vue
+++ app.vue
@@ -5,7 +5,9 @@ const { t, locale } = useI18n();
 console.log("Current locale:", locale.value);
 
 useHead(() => ({
-  title: locale.value === 'ar' ? 'مرحبًا' : 'Welcome'
-}))
-
+  htmlAttrs: {
+    dir: "rtl",
+  },
+  title: locale.value === "ar" ? "مرحبًا" : "Welcome",
+}));
 </script>
