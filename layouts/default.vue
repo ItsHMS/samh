054abb28c2c490f5b993ffb39ea076cc29514c6a
iamwaleedmirza <mirzawaleedbaig778@gmail.com>
2024-09-30 02:33:11 +0500

    feat(ui): completed homepage and default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
-  <header>Header</header>
-  <main>
-    <slot />
-  </main>
-  <footer>footer</footer>
+  <div class="flex h-screen bg-[#F3F7FC]">
+    <div class="flex-1 flex flex-col">
+      <AppHeader />
+      <main class="flex-1 bg-gray-200">
+        <div class="container mx-auto px-6 py-8">
+          <slot />
+        </div>
+      </main>
+      <AppFooter />
+    </div>
+  </div>
 </template>
+<script setup>
+import AppHeader from "~/components/app/AppHeader.vue";
+import AppFooter from "~/components/app/AppFooter.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    {{ t("welcome") + " to samh travels" }}
+    <div
+      :style="{
+        backgroundImage: `url(${mapImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }"
+    >
+      <HeroSection />
+      <SearchForm />
+    </div>
+    <PopularDestinations />
   </div>
 </template>
 <script setup>
+import HeroSection from "@/components/home/HeroSection.vue";
+import SearchForm from "@/components/home/SearchForm.vue";
+import PopularDestinations from "@/components/home/PopularDestinations.vue";
+import mapImage from "~/assets/images/map.png";
 const { t, locale } = useI18n();
 useHead(() => ({
   title: locale.value === "ar" ? "مرحبًا" : "Welcome",
